d2606fb1c27df9737425d5f2f166fae7d160b945
aioanedragos <aioane.costin@yahoo.com>
2022-01-26 18:31:46 +0200

    Integrare pagina de persoane (#5)
    * integrare comuna
    
    * Added service for insert person endpoint
    
    * probleme la baza de date
    "
    
    * Fixed insertPeople method and added migration for Persons table
    
    Co-authored-by: daniel <danybrench33@gmail.com>

---
+++ frontend/Data/Person.cs
@@ -0,0 +1,10 @@
+namespace wet_ui.Data
+{
+  public class Person
+  {
+    public int Id { get; set; }
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string email { get; set; }
+  }
+}
