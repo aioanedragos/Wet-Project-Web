3cf792618a80610ecef6c7622bae4a68ea92462f
daniel <danybrench33@gmail.com>
2022-01-26 18:35:48 +0200

    Added more endpoints to device controller

--- backend/Controllers/DeviceController.cs
+++ backend/Controllers/DeviceController.cs
@@ -16,11 +16,6 @@ public class DeviceController : ControllerBase
   [HttpGet]
   public async Task<ActionResult<List<Device>>> Get()
   {
-    // var devices = new List<Device> {
-    //   new Device("Light bulb", "RGB light bulb"),
-    //   new Device("Air conditioner")
-    // };
-
     var devices = _dbContext.Devices.ToList();
 
     return Ok(devices);
@@ -41,5 +36,51 @@ public class DeviceController : ControllerBase
     }
 
     return Ok(device);
+  }
+
+  [HttpPost]
+  public async Task<ActionResult<Device>> addDevice(Device device) {
+    device.Id = Guid.NewGuid();
+    this._dbContext.Devices.Add(device);
+    await _dbContext.SaveChangesAsync();
+    return Ok(device);
+  }
+
+  [HttpPut]
+  [Route("{deviceId}")]
+  public async Task<ActionResult<Device>> updateDevice(string deviceId, Device device) {
+    var isValidGuid = Guid.TryParse(deviceId, out var parsedId);
+    if (!isValidGuid) {
+      return BadRequest("Invalid id");
+    }
+
+    var dbDevice = await this._dbContext.Devices.FindAsync(parsedId);
+    if (dbDevice == null) {
+      return NotFound("Device not found");
+    }
+
+    dbDevice.Title = device.Title;
+    dbDevice.Description = device.Description;
+    await this._dbContext.SaveChangesAsync();
+
+    return Ok(device);
+  }
+
+  [HttpDelete]
+  [Route("{deviceId}")]
+  public async Task<ActionResult> deleteDevice(string deviceId) {
+    var isValidGuid = Guid.TryParse(deviceId, out var parsedId);
+    if (!isValidGuid) {
+      return BadRequest("Invalid id");
+    }
+
+    var dbDevice = await this._dbContext.Devices.FindAsync(parsedId);
+    if (dbDevice == null) {
+      return NotFound("Device not found");
+    }
+
+    this._dbContext.Devices.Remove(dbDevice);
+    await this._dbContext.SaveChangesAsync();
+    return Ok(true);
   }
 }
