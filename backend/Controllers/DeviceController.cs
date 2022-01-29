e6b2c805dfb3da87e73fbda9f5c4edf52c9081d0
daniel <danybrench33@gmail.com>
2022-01-29 15:46:26 +0200

    Changed add device to use a url to a thing description

--- backend/Controllers/DeviceController.cs
+++ backend/Controllers/DeviceController.cs
@@ -1,21 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
+using wet_api.Dtos;
 
 namespace wet_api.Controllers;
 
 [ApiController]
-[Route("devices")]
-public class DeviceController : ControllerBase 
+[Route("api/[controller]")]
+public class DeviceController : ControllerBase
 {
-  private readonly DataContext _dbContext;
-  public DeviceController(DataContext dataContext)
-  {
-      this._dbContext = dataContext;
-  }
+    private readonly DataContext _dbContext;
+    public DeviceController(DataContext dataContext)
+    {
+        this._dbContext = dataContext;
+    }
 
-  [HttpGet]
-  public async Task<ActionResult<List<Device>>> Get()
-  {
+    [HttpGet]
+    public async Task<ActionResult<List<Device>>> Get()
+    {
         var devices = new List<Device> {
        new Device("Light bulb", "RGB light buleqwedqwdbqwdbqwbdkqhwgdkqhwkdhkqwjhdkjqwhkjdhb"),
        new Device("Air conditioner"),
@@ -26,72 +27,89 @@ public class DeviceController : ControllerBase
        new Device("Light bulb", "RGB light bulb"),
        new Device("Air conditioner")
      };
-     
+
         //var devices = _dbContext.Devices.ToList();
 
-    return Ok(devices);
-  }
-
-  [HttpGet]
-  [Route("{deviceId}")]
-  public async Task<ActionResult<Device>> GetById(string deviceId)
-  {
-    var isValidGuid = Guid.TryParse(deviceId, out var parsedId);
-    if (!isValidGuid) {
-      return BadRequest("Invalid id");
+        return Ok(devices);
     }
 
-    var device = await _dbContext.Devices.FindAsync(parsedId);
-    if (device == null) {
-      return NotFound("Device not found");
+    [HttpGet]
+    [Route("{deviceId}")]
+    public async Task<ActionResult<Device>> GetById(string deviceId)
+    {
+        var isValidGuid = Guid.TryParse(deviceId, out var parsedId);
+        if (!isValidGuid)
+        {
+            return BadRequest("Invalid id");
+        }
+
+        var device = await _dbContext.Devices.FindAsync(parsedId);
+        if (device == null)
+        {
+            return NotFound("Device not found");
+        }
+
+        return Ok(device);
     }
 
-    return Ok(device);
-  }
-
-  [HttpPost]
-  public async Task<ActionResult<Device>> addDevice(Device device) {
-    device.Id = Guid.NewGuid();
-    this._dbContext.Devices.Add(device);
-    await _dbContext.SaveChangesAsync();
-    return Ok(device);
-  }
-
-  [HttpPut]
-  [Route("{deviceId}")]
-  public async Task<ActionResult<Device>> updateDevice(string deviceId, Device device) {
-    var isValidGuid = Guid.TryParse(deviceId, out var parsedId);
-    if (!isValidGuid) {
-      return BadRequest("Invalid id");
+    [HttpPost]
+    public async Task<ActionResult<Device>> addDevice(string url)
+    {
+        // device.Id = Guid.NewGuid();
+        // this._dbContext.Devices.Add(device);
+        // await _dbContext.SaveChangesAsync();
+        using (var httpClient = new HttpClient())
+        {
+            var data = await httpClient.GetFromJsonAsync<DeviceResponseDto>(new Uri(url));
+            System.Console.WriteLine(data);
+            var device = new Device(data.Title, data.Description);
+            this._dbContext.Devices.Add(device);
+            await _dbContext.SaveChangesAsync();
+            return Ok(device);
+        }
     }
 
-    var dbDevice = await this._dbContext.Devices.FindAsync(parsedId);
-    if (dbDevice == null) {
-      return NotFound("Device not found");
+    [HttpPut]
+    [Route("{deviceId}")]
+    public async Task<ActionResult<Device>> updateDevice(string deviceId, Device device)
+    {
+        var isValidGuid = Guid.TryParse(deviceId, out var parsedId);
+        if (!isValidGuid)
+        {
+            return BadRequest("Invalid id");
+        }
+
+        var dbDevice = await this._dbContext.Devices.FindAsync(parsedId);
+        if (dbDevice == null)
+        {
+            return NotFound("Device not found");
+        }
+
+        dbDevice.Title = device.Title;
+        dbDevice.Description = device.Description;
+        await this._dbContext.SaveChangesAsync();
+
+        return Ok(device);
     }
 
-    dbDevice.Title = device.Title;
-    dbDevice.Description = device.Description;
-    await this._dbContext.SaveChangesAsync();
+    [HttpDelete]
+    [Route("{deviceId}")]
+    public async Task<ActionResult> deleteDevice(string deviceId)
+    {
+        var isValidGuid = Guid.TryParse(deviceId, out var parsedId);
+        if (!isValidGuid)
+        {
+            return BadRequest("Invalid id");
+        }
 
-    return Ok(device);
-  }
+        var dbDevice = await this._dbContext.Devices.FindAsync(parsedId);
+        if (dbDevice == null)
+        {
+            return NotFound("Device not found");
+        }
 
-  [HttpDelete]
-  [Route("{deviceId}")]
-  public async Task<ActionResult> deleteDevice(string deviceId) {
-    var isValidGuid = Guid.TryParse(deviceId, out var parsedId);
-    if (!isValidGuid) {
-      return BadRequest("Invalid id");
+        this._dbContext.Devices.Remove(dbDevice);
+        await this._dbContext.SaveChangesAsync();
+        return Ok(true);
     }
-
-    var dbDevice = await this._dbContext.Devices.FindAsync(parsedId);
-    if (dbDevice == null) {
-      return NotFound("Device not found");
-    }
-
-    this._dbContext.Devices.Remove(dbDevice);
-    await this._dbContext.SaveChangesAsync();
-    return Ok(true);
-  }
 }
---
+++ backend/Dtos/DeviceJson.cs
@@ -0,0 +1,13 @@
+
+namespace wet_api.Dtos
+{
+
+    public class DeviceResponseDto
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public string href { get; set; }
+        public string Base { get; set; }
+    }
+}
