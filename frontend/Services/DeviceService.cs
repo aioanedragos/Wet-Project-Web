0bfb835cbf7c51d4817136cf36818cf826dbdb86
daniel <danybrench33@gmail.com>
2022-01-31 23:50:18 +0200

    Changed StringContent to JsonContent

--- frontend/Services/DeviceService.cs
+++ frontend/Services/DeviceService.cs
@@ -63,7 +63,7 @@ namespace wet_ui.Services
         else
         {
             this._httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            var content = new StringContent(value);
+            var content = JsonContent.Create<object>(value);
             var response = await this._httpClient.PatchAsync("/api/Devices/" + ID + "/properties/"+value, content);
             System.Console.WriteLine(response);
             return response.ToString();
