be5df1b72f13b464892edc213546180546e174cf
aioanedragos <aioane.costin@yahoo.com>
2022-02-01 20:52:27 +0200

    Test branch (#11)
    * ada
    
    * dsfs

--- frontend/Services/IPersonService.cs
+++ frontend/Services/IPersonService.cs
@@ -1,7 +1,9 @@
+using System.Text.Json;
 namespace wet_ui.Services 
 {
   public interface IPersonService 
   {
     public Task InsertPerson(Person person);
+    public Task<bool> GiveAcces(GiveAccessDto person);
   }
 }
--- frontend/Services/PersonService.cs
+++ frontend/Services/PersonService.cs
@@ -1,19 +1,39 @@
 using System.Net.Http.Json;
+using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace wet_ui.Services
 {
   public class PersonService: IPersonService
   {
     private readonly HttpClient _httpClient;
+    private ILocalStorageService _localStorageService;
 
-    public PersonService(HttpClient httpClient)
+
+    public PersonService(HttpClient httpClient, ILocalStorageService localStorageService)
     {
-      this._httpClient = httpClient;
+        this._httpClient = httpClient;
+        this._localStorageService = localStorageService;
     }
 
     public Task InsertPerson(Person person)
     {
       return this._httpClient.PostAsJsonAsync<Person>("api/Person/insertPeople", person);
+    }
+
+    public async Task<bool> GiveAcces(GiveAccessDto person)
+    {
+        var token = await _localStorageService.GetItem<string>("token");
+        if (token == null)
+        {
+            throw new Exception("No token found!");
+        }
+        else
+        {
+            this._httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var respons = await this._httpClient.PostAsJsonAsync<GiveAccessDto>("api/Person", person);
+            return true;
+        }
     }
   }
 }
