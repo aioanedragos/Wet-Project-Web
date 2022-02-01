fd9a14c3128c989b69837e4451c7774d89e1a56a
aioanedragos <aioane.costin@yahoo.com>
2022-02-01 20:12:37 +0200

    ada

---
+++ frontend/Data/GiveAccessDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+public enum ControlTypes
+{
+    OWNER = 0,
+    READ = 1,
+    READ_WRITE = 2
+}
+
+public class GiveAccessDto
+{
+    public string Email { get; set; }
+    public string DeviceId { get; set; }
+    public ControlTypes ControlType { get; set; }
+}
+
